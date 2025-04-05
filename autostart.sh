3459af62404f5d23f00e49a6250e03a1e8ccb55a
Prasanth K <prasanthc41m@pm.me>
2025-04-05 22:48:42 +0530

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
-sleep 15
-cd $HOME/.config/conky/ && conky -c conkyrc &>/dev/null
-cd $HOME/.config/conky/ && conky -c conky.conf &>/dev/null
+cd $HOME/.config/conky/ 
+conky -c conkyrc &>/dev/null
+conky -c conky.conf &>/dev/null
